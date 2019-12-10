383dbd6f1ea92518b95e9b8520a3ee86fdb19f75
Breno <breno.santosjc@gmail.com>
2019-12-10 10:01:41 -0300

    Security

--- app/controllers/home_secure_controller.rb
+++
@@ -1,9 +0,0 @@
-class HomeSecureController < SecureApplicationController
-
-  # 
-  # 
-  def index
-    @username, @senha = ActionController::HttpAuthentication::Basic::user_name_and_password(request)
-  end
-  
-end
