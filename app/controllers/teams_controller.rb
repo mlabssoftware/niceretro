fd48f170bc5ed198e1949dad2591944d9912436a
Palloi Hofmann <palloi.hofmann@gmail.com>
2020-05-28 01:11:11 -0300

    master - edit team

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,12 +2,21 @@ class TeamsController < ApplicationController
   respond_to :js, :html
 
   def index
-    @teams = Team.all
+    @teams = Team.order('name ASC')
     render layout: "pre_panel"
   end
 
   def new
     @team = Team.new
+  end
+
+  def edit
+    @team = Team.find(params[:id])
+  end
+
+  def update
+    @team = Team.find(params[:id])
+    @team.update_attributes(teams_params)
   end
 
   def create
