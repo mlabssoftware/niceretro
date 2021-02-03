bf0a05db9878786965097763096566aafedb3ec3
Yves Conselvan <yconselvan@yahoo.com.br>
2021-02-03 08:16:02 -0300

    SRE-219 | convert credentials to environment variables

--- app/controllers/concerns/http_auth_concern.rb
+++ app/controllers/concerns/http_auth_concern.rb
@@ -6,7 +6,7 @@ module HttpAuthConcern
     def http_authenticate
         return true unless Rails.env == 'production'
         authenticate_or_request_with_http_basic do |username, password|
-            username == 'mlabs' && password == 'mL4b$nice'
+            username == ENV['NICE_USER'] && password == ENV['NICE_PASSWORD']
         end
     end
 end
